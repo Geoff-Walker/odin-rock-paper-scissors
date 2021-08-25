bd221debdd35bde42ec29d2ed7609eed68bebd9e
Geoff-Walker <geoffwalker1980@gmail.com>
2021-08-25 13:16:34 +0100

    Script link basic logic outline

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+/////Rock paper scissors Objects
 const selection = {
 	name: [ 'rock', 'paper', 'scissors' ],
 	numVal: [ 0, 1, 2 ],
-	emoji: [ '👊', '✋', '✌️' ]
+	emoji: [ '👊', '✋', '✌️' ],
+	beats: [ 'scissors', 'rock', 'paper' ]
 };
+
+///////random computer pick
+
+let computerSelection = Math.trunc(Math.random() * 3);
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+
+///////If tie
+
+if (selection.name[computerSelection] === playerSelection) {
+	////// tie  add orange to the no one wins display wins leave selections alone
+	///// no score move on
+} else if (selection.beats[computerSelection] === playerSelection) {
+	////// you lose - red computer wins red border player and green border computer
+	///// add one to the score move on if less than 5
+} else {
+	////// you win - green wins green border player and red border computer
+	///// add one to the score move on if less than 5
+}
+
+console.log(selection.emoji[computerSelection]);
